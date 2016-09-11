4e355d00f54d92c81756a8caaf6f3ed3a15a3515
Bimal <asteriskbimal@gmail.com>
2016-09-11 17:17:39 -0500

    first Commit

---
+++ src/main/java/com/javacodegeeks/spring/jms/SpringJmsExample.java
@@ -0,0 +1,28 @@
+package com.javacodegeeks.spring.jms;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringJmsExample {
+	public static void main(String[] args) throws URISyntaxException, Exception {
+//		BrokerService broker = BrokerFactory.createBroker(new URI(
+//				"broker:(tcp://localhost:61616)"));
+//		broker.start();
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		try {
+
+			SpringJmsConsumer springJmsConsumer = (SpringJmsConsumer) context
+					.getBean("springJmsConsumer");
+			System.out.println("Consumer receives " + springJmsConsumer.receiveMessage());
+		} finally {
+//			broker.stop();
+//			context.close();
+		}
+	}
+}
